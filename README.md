56bc4fa76dd4c740f07fd1c62bd9d2e274108a31
Maya084 <37115588+Maya084@users.noreply.github.com>
2022-09-12 12:23:40 +0200

    🎨 Style: format code

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# canvas_js_projects
-HTML5 canvas + js animations
+# HTML5 canvas + js animations
 
-All of these are created by watching this: https://www.youtube.com/watch?v=EO6OkltgudE&list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTPXL
-
-I don't take any credit
+All of these are created by watching this: 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/EO6OkltgudE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- circular motion/canvas.js
+++ circular motion/canvas.js
@@ -24,56 +24,51 @@ addEventListener('resize', () => {
   init();
 })
 
-addEventListener("click", function() {
+addEventListener("click", function () {
   init();
 
 })
 
-function Particle(x,y,radius, color)
-{
+function Particle(x, y, radius, color) {
   this.x = x;
   this.y = y;
   this.radius = radius;
   this.color = color;
   this.radians = Math.random() * Math.PI * 2;
   this.velocity = 0.05;
-  var random = randomIntFromRange(80,120);
+  var random = randomIntFromRange(80, 120);
 
-    this.draw = function()
-    {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle=this.color;
-      c.fill();
-      c.closePath();
-    }
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
 
-    this.update = function()
-    {
-      this.x = x + Math.cos(this.radians)*random;
-      this.y = y + Math.sin(this.radians)*random;
-      this.radians += this.velocity;
-      this.draw();
-    }
+  this.update = function () {
+    this.x = x + Math.cos(this.radians) * random;
+    this.y = y + Math.sin(this.radians) * random;
+    this.radians += this.velocity;
+    this.draw();
+  }
 }
 
 let particles;
 
-
 function init() {
-    particles = [];
+  particles = [];
 
-    for(var i=0; i<50; i++)
-    {
-      particles.push(new Particle(canvas.width / 2, canvas.height / 2, 5, "#a8328b"));
-    }
+  for (var i = 0; i < 50; i++) {
+    particles.push(new Particle(canvas.width / 2, canvas.height / 2, 5, "#a8328b"));
+  }
 }
 
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  
+
   particles.forEach(particle => {
     particle.update();
   });
@@ -83,18 +78,16 @@ init();
 animate();
 
 function randomIntFromRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  
-  function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
-  
-  function distance(x1, y1, x2, y2) {
-    const xDist = x2 - x1;
-    const yDist = y2 - y1;
-  
-    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-  }
-  
-  
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function distance(x1, y1, x2, y2) {
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+}
